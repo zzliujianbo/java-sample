176f6361ed887c5b12878c7ef0761f40b4f50d98
刘jianbo <zzliujianbo@gmail.com>
2019-03-20 13:46:21 +0800

    add sample

---
+++ sample.java
@@ -0,0 +1,13 @@
+package com.hn;
+
+public class Sample {
+
+    public String name = "";
+
+    public int final MAX_STOCK_COUNT = 1;
+
+    public String getName() {
+        return this.name;
+    }
+
+}
