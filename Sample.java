29b2e2f1a3f0a53d8c46749cd8258cd245c37efd
刘jianbo <zzliujianbo@gmail.com>
2019-03-20 15:29:07 +0800

    add

---
+++ Sample.java
@@ -0,0 +1,47 @@
+package com.hn;
+
+public class Sample {
+
+    public String name = "";
+
+    public int final MAX_STOCK_COUNT = 1;
+
+    public long age = 1L;
+
+    public String[] args;
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * 获取单个对象
+     */
+    public Object getObject() {
+
+    }
+
+    /**
+     * 获取多个对象
+     */
+    public Object listObject() {
+
+    }
+
+    public int countObject() {
+
+    }
+
+    public int saveObject() {
+
+    }
+
+    public int removeObject() {
+
+    }
+
+    public int updateObject() {
+
+    }
+
+}
